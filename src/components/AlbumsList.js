d7d91c74da92ab440b0d32c569fa36d8e1006463
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-01-25 12:22:52 +0100

    Add actions in redux

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -4,13 +4,11 @@ export default function AlbumsList(props) {
     return (<div>
         <h1>All Albums</h1>
         <ul>
-                
             {props.albums.map(album =>
-             <li key={album.id}> {album.title} </li>
-)}
-            
+                <li key={album.id}> {album.title} </li>
+            )}
         </ul>
 
-        There are { props.albums.length} albums available.
+        There are {props.albums.length} albums available.
     </div>)
 }
